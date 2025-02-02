d69bc33ff2d6e424ba21a9e30347963debc3fafa
harshavardini <harshavardini8855@gmail.com>
2025-02-02 13:35:34 +0530

    permissionms

--- lib/utils/permissions.dart
+++ lib/utils/permissions.dart
@@ -1,9 +1,109 @@
-List<String> getUserPermissions(String role) {
+List<dynamic> getUserPermissions(String role) {
   if (role == "ADMIN") {
-    return ["LOCATIONS", "ADD USER"];
+    return [
+      {
+        "name":"Dashboard"
+      },
+      {
+        "name": "Master Data",
+        "options": [
+          {
+            "name": "Locations"
+          },
+          {
+            "name": "Countries"
+          },
+          {
+            "name": "Regions"
+          },
+          {
+            "name":"Standards"
+          },
+          {
+            "name":"Profile"
+          }
+        ]
+      },
+      {
+        "name": "User Management",
+        "options":[
+          {
+            "name": "Departments"
+          },
+          {
+            "name":"Users"
+          },
+          {
+            "name":"Roles"
+          }
+        ]
+      }
+
+    ];
   } else if (role == "MANAGER") {
-    return [""];
+    return [
+      {
+        "name":"Dashboard"
+      },
+      {
+        "name":"Master Data",
+        "options":[
+          {
+            "name":"Sectors"
+          },
+          {
+            "name":"Locations"
+          },
+          {
+            "name":"Countries"
+          },
+          {
+            "name":"Regions"
+          },
+          {
+            "name":"Standards"
+          },
+          {
+            "name":"ESG Goals and Targets"
+          },
+          {
+            "name":"ESG Categories"
+          },
+          {
+            "name":"My Goal Tracking"
+          },
+          {
+            "name":"SDG Goals"
+          }
+        ]
+
+      },
+      {
+        "name":"ESG Audit",
+      },
+      {
+        "name":"User Management"
+      }
+    ];
   } else {
-    return [];
+    return [
+      {
+        "name":"Dashboard"
+      },
+      {
+        "name":"Master Data",
+        "options":[
+          {
+            "name":"Standards"
+          },
+          {
+            "name":"Goal Tracking"
+          }
+        ]
+      },
+      {
+        "name":"ESG Audit"
+      }
+    ];
   }
 }
